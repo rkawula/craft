73896ae519decf5c565cfe861c2e5880d18b8aea
Rachel Kawula <rkawula@gmail.com>
2019-01-24 02:25:43 -0800

    Removed deprecated parameter from App.

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
                       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
                       crossOrigin="anonymous"/>
-                <BusinessDashboard bankData={this.state.data}/>
+                <BusinessDashboard />
             </div>
         );
     }
